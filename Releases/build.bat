052812a1e9b42165551e4935d75b1f1639a442fe
EliteMasterEric <ericmyllyoja@gmail.com>
2023-12-08 14:29:49 -0500

    Fix to build script to remove recursion

--- Releases/build.bat
+++ Releases/build.bat
@@ -10,4 +10,4 @@ REM Copy all files from ../Coroner/build/bin/Debug to the current directory
 xcopy /s /y /q ..\Coroner\build\bin\Debug\* .
 
 REM Create a zip file named Coroner.zip containing all files (except build.bat) in the current directory
-"C:\Program Files\7-Zip\7z.exe" a Coroner.zip * -x!build.bat
+"C:\Program Files\7-Zip\7z.exe" a Coroner.zip * -x!build.bat -x!Coroner.zip
